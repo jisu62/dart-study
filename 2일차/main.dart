ebcfce44fb205734452a9dc07f07ba1e32bb2e61
jisu62 <gs108403@naver.com>
2025-07-08 17:09:24 +0900

    3번 퀴즈

--- 2일차/main.dart
+++ 2일차/main.dart
@@ -1,23 +1,10 @@
-double calculateBMI(double heightCm, double weightKg) {
-  double heightM = heightCm / 100;
-  return weightKg / (heightM * heightM);
+void printUserInfo(String name, String phone, [int? age]) {
+  String ageText = (age == null) ? '비공개' : '$age';
+  print('제 이름은 $name, 전화번호는 $phone이며 나이는 $ageText입니다');
 }
 
 void main() {
-  double height = 176;
-  double weight = 63;
-  double bmi = calculateBMI(height, weight);
-  String status;
-
-  if (bmi < 18.5) {
-    status = '저체중';
-  } else if (bmi <= 22.9) {
-    status = '정상';
-  } else if (bmi <= 24.9) {
-    status = '과체중';
-  } else {
-    status = '비만';
-  }
-
-  print('영재의 BMI 점수는 ${bmi.toStringAsFixed(1)}점이며 현재 $status 상태');
+  // 예시 호출
+  printUserInfo('홍길동', '010-1234-5678', 25); // 나이 입력한 경우
+  printUserInfo('이영희', '010-9876-5432'); // 나이 미입력 시
 }
