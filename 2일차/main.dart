d2f021dfa4651ebf28d5f5b2cd82e6bf9d1bae11
jisu62 <gs108403@naver.com>
2025-07-08 17:17:18 +0900

    4번퀴즈 for문

--- 2일차/main.dart
+++ 2일차/main.dart
@@ -1,10 +1,9 @@
-void printUserInfo(String name, String phone, [int? age]) {
-  String ageText = (age == null) ? '비공개' : '$age';
-  print('제 이름은 $name, 전화번호는 $phone이며 나이는 $ageText입니다');
-}
-
 void main() {
-  // 예시 호출
-  printUserInfo('홍길동', '010-1234-5678', 25); // 나이 입력한 경우
-  printUserInfo('이영희', '010-9876-5432'); // 나이 미입력 시
+  for (int i = 1; i <= 40; i++) {
+    if (i.toString().contains('3')) {
+      print('짝');
+    } else {
+      print(i);
+    }
+  }
 }
