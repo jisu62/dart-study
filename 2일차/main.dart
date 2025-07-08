c42cac535d284e4d2d990e963d26c400ac91b5a5
jisu62 <gs108403@naver.com>
2025-07-08 14:41:31 +0900

    짝수 구구단 만들기

--- 2일차/main.dart
+++ 2일차/main.dart
@@ -1,7 +1,11 @@
 void main() {
-  // 구구단 1단 출력
-  for (int i = 1; i <= 9; i++) {
-    print('1 x $i = ${1 * i}');
+  for (int dan = 2; dan <= 9; dan++) {
+    if (dan % 2 == 0) {
+      print('== $dan단 ==');
+      for (int i = 1; i <= 9; i++) {
+        print('$dan x $i = ${dan * i}');
+      }
+      print(''); // 단 사이 줄바꿈
+    }
   }
 }
-
